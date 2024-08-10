82b4c98a419f8bdaa967b4f0cb57fe0fc5debdab
ThomasGroove <s4040269@rmit.edu.vn>
2024-08-10 21:11:18 +0700

    start.cpp

--- start.cpp
+++ start.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main(){
     std::cout << "Hello world!";
     std::cout << "Branch Marcio";
     std::cout << "Test feature";
+    cout << "Hello bro";
     return 0;
 }
