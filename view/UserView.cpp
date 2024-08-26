7a1252d1c9749eb0b7a634a86de0c95317a806cc
ThomasGroove <s4040269@rmit.edu.vn>
2024-08-26 11:26:50 +0700

    Update UserView.cpp

--- view/UserView.cpp
+++ view/UserView.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <vector>
-class UserView{
+class UserView : public User {
 public:
-    void displayUserProfile(User* user){
+    // Constructor that calls the User constructor
+    UserView(string uName, string pwd, string fName, string pNumber, string mail, string idT, string idNum, int credits)
+        : User(uName, pwd, fName, pNumber, mail, idT, idNum, credits) {}
 
+    // Method to view the user profile
+    void displayUserProfile(User* user) 
+    {
+        cout << "User Profile:\n";
+        cout << "Your Username: " << user->getUserName() << "\n";
+        cout << "Your Full Name: " << user->getFullName() << "\n";
+        cout << "Your Phone Number: " << user->getPhoneNumber() << "\n";
+        cout << "Your Email: " << user->getEmail() << "\n";
+        cout << "Your Credit Points: " << user->getCreditPoints() << "\n";
     }
     void displayCarpoolListings(std::vector <CarpoolListing*> listings){
 
